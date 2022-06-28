bcd7b3c92f97314ad2e0eca815d2fd92453ef0b2
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-06-28 16:30:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# URIJudge_AreaCirculo
+ Exercicio URI Judge que calcula area do circulo
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        float area=0;
+        System.out.println("A="+area);
+    }
+}
